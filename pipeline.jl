41c32f54028386723e9499603982d715a2c23ad2
andrew-saydjari <saydjari@g.harvard.edu>
2024-08-14 13:40:37 -0600

    clean sirs.jl implementation, with refarray

--- pipeline.jl
+++ pipeline.jl
@@ -11,7 +11,7 @@ t_now = now(); dt = Dates.canonicalize(Dates.CompoundPeriod(t_now-t_then)); prin
     BLAS.set_num_threads(1)
     using FITSIO, HDF5, FileIO, JLD2
     using DataFrames, EllipsisNotation, StatsBase
-    using ParallelDataTransfer
+    using ParallelDataTransfer, SIRS
 
     src_dir = "./"
     include(src_dir*"src/ap3D.jl")
@@ -24,8 +24,13 @@ using LibGit2; git_branch, git_commit = initalize_git(src_dir); @passobj 1 worke
 ## some hard coded parameters
 
 ##### 3D stage
-function process_3D(release_dir,mjd,expid)
-    f = h5open("$(mjd).h5")
+function process_3D(release_dir,mjd,expid,outdir;firstind=2)
+    dirName = outdir*"/ap2D/"
+    if !ispath(dirName)
+        mkpath(dirName)
+    end
+
+    f = h5open(outdir*"almanac/$(mjd).h5")
     df = DataFrame(read(f["apo/$(mjd)/exposures"]))
     close(f)
 
@@ -34,33 +39,43 @@ function process_3D(release_dir,mjd,expid)
     readVarMat = 25*ones(Float32,2560,2048)
     # ADD load the dark currrent map
     # load SIRS.jl models
-    # sirs4amps = SIRS.restore("sirs_test_d12_r60_n15.jld");
-    # sirsrefas2 = SIRS.restore("sirs_test_ref2_d12_r60_n15.jld");
+    sirs4amps = SIRS.restore(outdir*"cal/sirs_test_d12_r60_n15.jld");
+    sirsrefas2 = SIRS.restore(outdir*"cal/sirs_test_ref2_d12_r60_n15.jld");
+    # write out sym links in the level of folder that MUST be uniform in their cals? or a billion symlinks with expid
 
     # this is only doing chip A for now (because of almanac)
     rawpath = build_raw_path(release_dir,df.observatory[expid],df.mjd[expid],df.chip[expid],df.exposure[expid])
     # decompress and convert apz data format to a standard 3D cube of reads
     cubedat, hdr = apz2cube(rawpath);
 
-    # ADD? reset anomaly fix
+    # ADD? reset anomaly fix (currently just drop first ind as our "fix")
+    tdat = @view cubedat[:,:,firstind:end]
 
-    # remove 1/f correlated noise (using SIRS.jl)
-    # fixing the reference array is a bit of a hack right now (IRRC might fix)
-
+    ## remove 1/f correlated noise (using SIRS.jl) [some preallocs would be helpful]
+    in_data = Float64.(tdat[1:2048,:,:]);
+    outdat = zeros(Float64,size(tdat,1),size(tdat,2),size(in_data,3))
+    sirssub!(sirs4amps, in_data, f_max=95.);
+    outdat[1:2048,:,:] .= in_data
+    
+    # fixing the 1/f in the reference array is a bit of a hack right now (IRRC might fix)
+    in_data = Float64.(tdat[1:2048,:,:]);
+    in_data[513:1024,:,:].=tdat[2049:end,:,:]
+    sirssub!(sirsrefas2, in_data, f_max=95.);
+    outdat[2049:end,:,:] .= in_data[513:1024,:,:]
 
     # ADD? reference array-based masking/correction
 
     # ADD? nonlinearity correction
 
     # extraction 3D -> 2D
-    dimage, ivarimage = dcs(cubedat,gainMat,readVarMat,firstind=2);
+    dimage, ivarimage = dcs(outdat,gainMat,readVarMat,firstind=1);
 
     # dark current subtraction
 
     # need to clean up exptype to account for FPI versus ARCLAMP
     outfname = join(["ap2D",df.observatory[expid],df.mjd[expid],df.chip[expid],df.exposure[expid],df.exptype[expid]],"_")
     # probably change to FITS to make astronomers happy (this JLD2, which is HDF5, is just for debugging)
-    jldsave(outfname*".jld2"; dimage, ivarimage)
+    jldsave(outdir*"/ap2D/"*outfname*".jld2"; dimage, ivarimage)
 end
 
 ## Parse command line arguments
@@ -79,11 +94,16 @@ function parse_commandline()
             help = "exposure number to be run"
             arg_type = Int
             default = 1
+        "--outdir"
+            required = true
+            help = "output directory"
+            arg_type = String
+            default = "../outdir/"
     end
     return parse_args(s)
 end
 
 parg = parse_commandline()
 release_dir = "sdsswork" # hardcoding for now, 3D raws never move so we could leave this out
-process_3D(release_dir,parg["mjd"],parg["expid"])
+process_3D(release_dir,parg["mjd"],parg["expid"],parg["outdir"])
 
