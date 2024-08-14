0ad3b74ba43d2f5a28fced8ffac0b7e60a08e886
andrew-saydjari <saydjari@g.harvard.edu>
2024-08-14 12:01:02 -0600

    push working

--- src/utils.jl
+++ src/utils.jl
@@ -6,3 +6,38 @@ function initalize_git(git_dir)
     println("Running on branch: $git_branch, commit: $git_commit"); flush(stdout)
     return git_branch, git_commit
 end
+
+function isnanorzero(x)
+    return isnan(x) | iszero(x)
+end
+
+nanzeromean(x) = if all(isnanorzero,x)
+    NaN
+else
+    mean(filter(!isnanorzero,x))
+end
+nanzeromean(x,y) = mapslices(nanzeromean,x,dims=y)
+
+nansum(x) = sum(filter(!isnan,x))
+nansum(x,y) = mapslices(nansum,x,dims=y)
+
+nanzerosum(x) = if all(isnanorzero,x)
+    NaN
+else
+    sum(filter(!isnanorzero,x))
+end
+nanzerosum(x,y) = mapslices(nanzerosum,x,dims=y)
+
+nanzeromedian(x) = if all(isnanorzero,x)
+    NaN
+else
+    median(filter(!isnanorzero,x))
+end
+nanzeromedian(x,y) = mapslices(nanzeromedian,x,dims=y)
+
+nanzeroiqr(x) = if all(isnanorzero,x)
+    NaN
+else
+    iqr(filter(!isnanorzero,x))/1.34896
+end
+nanzeroiqr(x,y) = mapslices(nanzeroiqr,x,dims=y)
