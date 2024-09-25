ba4a66e6d90d0596a5f79d38e6fd8a4dbdce8a54
Adam Wheeler <wheeler.883@osu.edu>
2024-09-25 14:32:51 -0600

    extremely basic validation

--- file_schema/validate.jl
+++ file_schema/validate.jl
@@ -1,17 +1,37 @@
 using YAML, HDF5
 
+"""
+Checks that a file matches a spec.
+"""
 function validate(path, spec_path)
     spec = YAML.load_file(spec_path)
     layout = h5open(h5structure, path, "r") 
 
-    # TODO make sure these strctures are the same
+    @show spec
+    println()
+    @show layout
+
+    # TODO validate filename?
     # TODO make sure everything is in the glossary
+
+    same_up_to_order(spec["structure"], layout)
 end
 
-function h5structure(h::Union{HDF5.File, HDF5.Group})
-    Dict([k=>h5structure(h[k]) for k in keys(h)])
-end
-function h5structure(h::HDF5.Dataset)
-    #TODO
+function h5structure(h::HDF5.File)
+    map(keys(h)) do k
+        if h[k] isa HDF5.Dataset
+            # TODO handle checking structure of info inside datasets
+            k
+        else
+            #TODO handle groups
+        end
+    end
 end
 
+function same_up_to_order(spec, actual)
+    # TODO recursively handle groups, etc.
+    sort(spec) == sort(actual)
+end
+
+println(validate("/uufs/chpc.utah.edu/common/home/sdss42/sdsswork/users/u6039752-1/working/2024_09_24/outdir/apred/59142/ap2D_apo_59142_a_35800020_INTERNALFLAT.jld2", 
+"files/ap2D.yaml"))
