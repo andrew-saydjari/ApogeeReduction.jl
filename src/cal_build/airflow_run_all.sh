d72dd2690482cd924a1d93d72c47ede570387cb0
Andy Casey <andycasey@gmail.com>
2024-10-07 12:56:46 -0600

    correct branch

--- src/cal_build/airflow_run_all.sh
+++ src/cal_build/airflow_run_all.sh
@@ -26,16 +26,18 @@ export SLURM_JOB_NODELIST=$SLURM_NODELIST
 
 echo `printenv`
 
-runname="all_${mjd_start}_${mjd_end}"
+runname="${mjd_start}_${mjd_end}"
 outdir="../outdir/${mjd_start}_${mjd_end}/"
 doutdir=$outdir
 almanac_file=${outdir}almanac/${runname}.h5
-runlist=${outdir}almanac/runlist_${runname}.jld2
+
 
 module load sdssdb/main sdsstools postgresql ffmpeg
 
 echo `module list`
-julia +1.10.0 --project="./" src/cal_build/make_runlist.jl --tele $tele --almanac_file $almanac_file --output $runlist
+
+runlist=${outdir}almanac/runlist_dark_cal_${runname}.jld2
+julia +1.10.0 --project="./" src/cal_build/make_runlist_darks.jl --tele $tele --almanac_file $almanac_file --output $runlist
 
 # run the reduction pipeline (all cals like dark sub/flats that would be a problem, should be post 3D->2D extraction)
 julia +1.10.0 --project="./" pipeline.jl --tele $tele --runlist $runlist --outdir $doutdir --runname $runname --chip "a"
@@ -46,6 +48,14 @@ julia +1.10.0 --project="./" pipeline.jl --tele $tele --runlist $runlist --outdi
 julia +1.10.0 --project="./" src/cal_build/make_stack_darks.jl --mjd-start $mjd_start --mjd-end $mjd_end --tele $tele --chip "a" --dark_dir $doutdir
 julia +1.10.0 --project="./" src/cal_build/make_stack_darks.jl --mjd-start $mjd_start --mjd-end $mjd_end --tele $tele --chip "b" --dark_dir $doutdir
 julia +1.10.0 --project="./" src/cal_build/make_stack_darks.jl --mjd-start $mjd_start --mjd-end $mjd_end --tele $tele --chip "c" --dark_dir $doutdir
+
+runlist=${outdir}almanac/runlist_flat_cal_${runname}.jld2
+julia +1.10.0 --project="./" src/cal_build/make_runlist_internal_flats.jl --tele $tele --almanac_file $almanac_file --output $runlist
+
+# run the reduction pipeline (all cals like dark sub/flats that would be a problem, should be post 3D->2D extraction)
+julia +1.10.0 --project="./" pipeline.jl --tele $tele --runlist $runlist --outdir $doutdir --runname $runname --chip "a"
+julia +1.10.0 --project="./" pipeline.jl --tele $tele --runlist $runlist --outdir $doutdir --runname $runname --chip "b"
+julia +1.10.0 --project="./" pipeline.jl --tele $tele --runlist $runlist --outdir $doutdir --runname $runname --chip "c"
 
 # Clean up logs and Report Timing
 formatted_time=$(printf '%dd %dh:%dm:%ds\n' $(($SECONDS/86400)) $(($SECONDS%86400/3600)) $(($SECONDS%3600/60)) $(($SECONDS%60)))
