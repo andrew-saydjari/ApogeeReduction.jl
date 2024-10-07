b54cdea189ec6cee5f65e1fd2a4f810fd2859786
Andrew Saydjari <andrew.saydjari@princeton.edu>
2024-10-07 10:27:09 -0400

    Update src/cal_build/make_stack_flats.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/cal_build/make_stack_flats.jl
+++ src/cal_build/make_stack_flats.jl
@@ -129,8 +129,8 @@ bad_pix_dark = (dark_pix_bitmask[5:2044,5:2044] .& bad_dark_pix_bits .!= 0);
     bad_pix .|= (bmat./ref_med.<pcut_flat)
     good_pix = .!grow_msk2d(bad_pix; rad=3)[:]
 
-    cvec = design_matrix[good_pix,:] \ b[good_pix]
-    modImage = reshape(design_matrix*cvec,nx,ny);
+    cvec = design_matrix[good_pix, :] \ b[good_pix]
+    modImage = reshape(design_matrix * cvec, nx, ny)
 
     flat_im .+= bmat ./ modImage
     model_im .+= modImage
