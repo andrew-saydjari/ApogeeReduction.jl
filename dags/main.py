9f68285e6791d1281269e5d68c4a65e4ff107a4a
Andy Casey <andycasey@gmail.com>
2024-10-07 12:45:12 -0600

    dag refactoring

--- dags/main.py
+++ dags/main.py
@@ -1,130 +1,164 @@
 import os
 import re
+from datetime import datetime, timedelta
+from astropy.time import Time
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from airflow.operators.python import PythonOperator
-from airflow.exceptions import AirflowSkipException
-from datetime import datetime, timedelta
+from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.utils.task_group import TaskGroup
+from airflow.exceptions import AirflowSkipException
 
-from airflow.sensors.base_sensor_operator import BaseSensorOperator
-from subprocess import check_output, Popen, PIPE
+from slurm_utils import SlurmJobStatusSensor
 
-slurm_partition = "sdss-np"
-slurm_job_name_template = "DreamTeam-{{ ds }}"
+SLURM_CLUSTER = nk, *_ = "notchpeak"
+REPO_DIR = "$MWM_SANDBOX/ApogeeReduction.jl"
+REPO_BRANCH = "main"
 
-SLURM_ALLOC_KWARGS = re.sub(r"\s+", " ", 
+OUTPUT_DIR_TEMPLATE = (
+    "$MWM_SANDBOX/"
+    "outdir/"
+    "{{ ti.xcom_pull(task_ids='setup.mjd_start') }}_{{ ti.xcom_pull(task_ids='setup.mjd_end') }}/"
+)
+
+ALMANAC_PATH = f"{OUTPUT_DIR_TEMPLATE}/almanac/all_{{{{ ti.xcom_pull(task_ids='setup.mjd_start') }}}}_{{{{ ti.xcom_pull(task_ids='setup.mjd_end') }}}}.h5"
+
+sbatch_kwargs = re.sub(r"\s+", " ", 
     f"""
     -vvv
-    --job-name={slurm_job_name_template}
-    --account={slurm_partition} 
-    --partition={slurm_partition}
-    --nodes=1
-    --ntasks=1
-    --mem-per-cpu=100M
-    --cpus-per-task=1
+    --job-name=DreamTeam-{{{{ ds }}}}
+    --account=sdss-{nk}p 
+    --partition=sdss-{nk}p
+    --clusters={SLURM_CLUSTER}
+    --nodes=2
+    -o {OUTPUT_DIR_TEMPLATE}/logs/vmstat_{{{{ ds }}}}_{{{{ ti.run_id }}}}.out
+    -e {OUTPUT_DIR_TEMPLATE}/logs/vmstat_{{{{ ds }}}}-{{{{ ti.run_id }}}}.err
+    --ntasks=128
+    --exclusive
+    --mem=0
     --time=01:00:00
     """.replace("\n", " ")
 )
-
-# TODO: Put this slurm stuff elsewhere, and make it not require --account or a full path
-def get_slurm_queue():
-    pattern = (
-        "(?P<id>\d+)+\s+(?P<name>[-\w\d_\.]+)\s+(?P<user>[\w\d]+)\s+(?P<group>\w+)"
-        "\s+(?P<account>[-\w]+)\s+(?P<partition>[-\w]+)\s+(?P<time_limit>[-\d\:]+)\s+"
-        "(?P<time_left>[-\d\:]+)\s+(?P<status>\w*)\s+(?P<nodelist>[\w\d\(\)]+)"
-    )
-    process = Popen(
-        [
-            "/uufs/notchpeak.peaks/sys/installdir/slurm/std/bin/squeue",
-            "--account=sdss-np,sdss-kp,notchpeak-gpu,sdss-np-fast",
-            '--format="%14i %j %10u %10g %13a %13P %11l %11L %2t %R"',
-        ],
-        stdin=PIPE,
-        stdout=PIPE,
-        stderr=PIPE,
-        universal_newlines=True,
-    )
-    output, error = process.communicate()
-    yield from (match.groupdict() for match in re.finditer(pattern, output))
-    
-
-class SenseSlurmJobReadiness(BaseSensorOperator):
-
-    template_fields = ("job_id", "job_name")
-
-    def __init__(
-        self,
-        job_id: str | int = None,
-        job_name: str = None,
-        poke_interval: int = 60,
-        exponential_backoff: bool = True,
-        timeout: int = 86_400,
-        soft_fail: bool = True,
-        **kwargs
-    ) -> None:
-        super().__init__(
-            mode="poke", 
-            poke_interval=poke_interval,
-            exponential_backoff=exponential_backoff,
-            timeout=timeout,
-            soft_fail=soft_fail,
-            **kwargs
-        )
-        if job_id is None and job_name is None:
-            raise ValueError("Either job_id or job_name must be provided")
-        self.job_name = job_name
-        self.job_id = job_id
-        return None
+srun_prefix = re.sub(r"\s+", " ", f"""
+    srun 
+    -v
+    --external-launcher
+    --ntasks=1
+    --ntasks-per-node=16
+    --jobid={{{{ ti.xcom_pull(task_ids='slurm.submit') }}}} 
+    -M {SLURM_CLUSTER}
+    -D {REPO_DIR}
+    --export=ALL,SLURM_TASKS_PER_NODE=64\(x2\),SLURM_JOB_QOS=sdss-{nk}p,SLURM_JOB_ACCOUNT=sdss-{nk}k,SLURM_NPROCS=128,SLURM_STEP_NUM_TASKS=128,SLURM_NTASKS=32,SLURM_NNODES=2,SLURM_JOB_NUM_NODES=2,SLURM_STEP_NUM_NODES=2,SLURM_CLUSTERS={SLURM_CLUSTER},SLURM_CLUSTER={SLURM_CLUSTER}.peaks,
+    --mem=400G
+""") 
 
 
-    def poke(self, context) -> bool:
-        for job in get_slurm_queue():
-            if (
-                (self.job_id is not None and f"{self.job_id}" == job["id"])
-            or  (self.job_name is not None and self.job_name == job["name"])
-            ):
-                return (job["status"] == "R")
-        # If we get to this point, the job is not even in the list, so we should fail or skip
-        raise AirflowSkipException(f"Job not found in queue (id={self.job_id}, name={self.job_name})")
-
+def observatories_with_data(almanac_path, **kwargs):
+    import h5py as h5
+    with h5.File(os.path.expandvars(almanac_path)) as fp:
+        if len(fp.keys()) == 0:
+            raise AirflowSkipException(f"No data in {almanac_path}")
+        return list(fp.keys())
 
 with DAG(
     "ApogeeReduction-main", 
-    start_date=datetime(2023, 11, 13), 
-    schedule_interval="@daily",
+    #start_date=datetime(2014, 7, 18), 
+    start_date=datetime(2023, 9, 1),
+    schedule_interval="@monthly",
     max_active_runs=1,
-    catchup=False,
+    catchup=True,
 ) as dag:
-
     with TaskGroup(group_id="update") as group_git:
-        p = BashOperator(
-            task_id="git_pull",
-            bash_command=f"cd $MWM_SANDBOX/ApogeeReduction.jl; git checkout main; git pull",
+        (
+            BashOperator(
+                task_id="repo",
+                bash_command=(
+                    f"cd {REPO_DIR}; "
+                    f"git checkout {REPO_BRANCH}; "
+                    "git pull"
+                )
+            )
+        ) >> (
+            BashOperator(
+                task_id="dag",
+                bash_command="airflow dags report",
+            )
         )
-        r = BashOperator(
-            task_id="refresh_dag",
-            bash_command="airflow dags report"
+
+    with TaskGroup(group_id="setup") as group_setup:
+        [
+            PythonOperator(
+                task_id="mjd_start",
+                python_callable=lambda data_interval_start, **_: int(Time(data_interval_start).mjd)
+            ),
+            PythonOperator(
+                task_id="mjd_end",
+                python_callable=lambda data_interval_end, **_: int(Time(data_interval_end).mjd)
+            )
+        ] >> (
+            BashOperator(
+                task_id="mkdirs",
+                bash_command=(
+                    f"mkdir -p {OUTPUT_DIR_TEMPLATE};"
+                    f"mkdir -p {OUTPUT_DIR_TEMPLATE}/almanac;"
+                    f"mkdir -p {OUTPUT_DIR_TEMPLATE}/logs;"
+                    f"mkdir -p {OUTPUT_DIR_TEMPLATE}/darks;"
+                )
+            )        
+        ) >> (
+            BashOperator(
+                task_id="almanac",
+                bash_command=(
+                    "module load almanac sdssdb/main sdsstools postgresql; "
+                    "almanac -v -p 12 --apo --lco "
+                    "--mjd-start {{ ti.xcom_pull(task_ids='setup.mjd_start') }} "
+                    "--mjd-end {{ ti.xcom_pull(task_ids='setup.mjd_end') }} "
+                    f"--output {ALMANAC_PATH}"
+                )
+            )
+        ) >> (            
+            PythonOperator(
+                task_id="any_data",
+                python_callable=observatories_with_data,
+                op_kwargs=dict(almanac_path=f"{ALMANAC_PATH}")
+            )
         )
-        p >> r
 
     with TaskGroup(group_id="slurm") as group_slurm:
+        (
+            BashOperator(
+                task_id="submit", 
+                bash_command=f"sbatch {sbatch_kwargs} --wrap=\"vmstat -t 1\"",
+                output_processor=lambda o: re.match("Submitted batch job (\d+)", o).group(1)
+            )
+        ) >> (
+            SlurmJobStatusSensor(
+                task_id="wait", 
+                job_id="{{ ti.xcom_pull(task_ids='slurm.submit') }}",
+            )
+        )
 
-        salloc = BashOperator(task_id="allocate", bash_command=f"module load slurm/notchpeak; salloc {SLURM_ALLOC_KWARGS} /usr/bin/sleep 60 </dev/null &")
-        wait = SenseSlurmJobReadiness(task_id="wait", job_name=slurm_job_name_template)
-        salloc >> wait
-
-
-    # Need to wait for the job to be allocated
-    test_srun = BashOperator(
-        task_id="test_srun",
-        bash_command=f"srun --job-name={slurm_job_name_template} --mem=10mb bash -c \"echo 'Hello world ' `hostname`\"",
+    branch = BranchPythonOperator(
+        task_id="branch",
+        python_callable=observatories_with_data,
+        op_kwargs=dict(almanac_path=f"{ALMANAC_PATH}")
     )
 
-    test_failure = BashOperator(
-        task_id="test_failure",
-        bash_command=f"srun --job-name={slurm_job_name_template} --mem=10mb python -c \"1/0\""
+    observatories = []
+    for observatory in ("apo", "lco"):
+        with TaskGroup(group_id=observatory) as group:                
+            with TaskGroup(group_id="calibrations") as g:
+                BashOperator(
+                    task_id="3d",
+                    bash_command=f"{srun_prefix} src/cal_build/airflow_run_all.sh {observatory} {{{{ ti.xcom_pull(task_ids='setup.mjd_start') }}}} {{{{ ti.xcom_pull(task_ids='setup.mjd_end') }}}} {OUTPUT_DIR_TEMPLATE} {ALMANAC_PATH}",
+                )
+            
+        observatories.append(g)
+
+    scancel = BashOperator(
+        task_id="teardown",
+        bash_command=f"scancel -M {SLURM_CLUSTER} {{{{ ti.xcom_pull(task_ids='slurm.submit') }}}}",
+        trigger_rule="all_done",
     )
 
-
-    group_git >> group_slurm >> (test_srun, test_failure)
+    group_git >> group_setup >> group_slurm >> branch >> observatories >> scancel
+    
