ee07568aa55b7f03e8912b29382104bd4d5faded
andrew-saydjari <saydjari@g.harvard.edu>
2024-10-07 08:18:52 -0600

    working flats

--- src/utils.jl
+++ src/utils.jl
@@ -48,3 +48,44 @@ nanzeroiqr(x) =
         iqr(filter(!isnanorzero, x)) / 1.34896
     end
 nanzeroiqr(x, y) = mapslices(nanzeroiqr, x, dims = y)
+
+function grow_msk2d(msk; rad=1)
+    (sx, sy) = size(msk)
+    msknew = zeros(Bool,(sx,sy))
+    for i=1:sx
+        for j=1:sy
+            srngx = maximum([1,i-rad]):minimum([sx,i+rad])
+            srngy = maximum([1,j-rad]):minimum([sy,j+rad])
+            msknew[i,j] = any(msk[srngx,srngy])
+        end
+    end
+    return msknew
+end
+
+function gen_design_mat(nx,ny,fx,fy,X,Y)
+    n_points = nx * ny
+    n_basis = 2*(fx + 1) * (fy + 1) - 2 + 3 #+ ny  # Total number of basis functions (sin + cos + constant)
+    design_matrix = zeros(Float64, n_points, n_basis)
+
+    col = 1
+    for kx in 0:fx
+        for ky in 0:fy
+            if !((kx==0) & (ky==0))
+                # Sine basis functions
+                design_matrix[:, col] = vec(sin.(kx .* X + ky .* Y))
+                col += 1
+
+                # Cosine basis functions
+                design_matrix[:, col] = vec(cos.(kx .* X + ky .* Y))
+                col += 1
+            end
+        end
+    end
+    design_matrix[:,col].=1
+    col += 1
+    design_matrix[:,col].=vec(X./maximum(X))
+    col += 1
+    design_matrix[:,col].=vec(Y./maximum(Y))
+    col += 1;
+    return design_matrix
+end
