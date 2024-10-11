2ac3fdf2c8bffb9a504e6c768ebbc1922e5ad31d
Andy Casey <andycasey@gmail.com>
2024-10-11 14:18:45 -0600

    remove superfluous msk2

--- src/ap2Dcal.jl
+++ src/ap2Dcal.jl
@@ -21,13 +21,11 @@ function get_cal_path(df, tele, mjd, chip)
     mskTeleChip = (df.telescope .== tele) .& (df.chip .== chip)
     msk0 = mskTeleChip .& (df.mjdstart .<= mjd .<= df.mjdend)
     msk1 = mskTeleChip .& (mjd .<= df.mjdstart)
-    msk2 = mskTeleChip .& (df.mjdend .<= mjd)
     if count(msk0) > 0
         return df.path[findlast(msk0)], 0
     elseif count(msk1) > 0
         return df.path[findlast(msk1)], 2^1 # flag for using cal from after exposure
     else
-        count(msk2) > 0
-        return df.path[findfirst(msk2)], 2^2 # flag for using cal from before exposure
+        return df.path[findfirst(mskTeleChip)], 2^2 # flag for using cal from before exposure
     end
 end
