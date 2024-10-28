761505b1455c3adf0c4de9bed99eb75b9504ecf2
andrew-saydjari <saydjari@g.harvard.edu>
2024-10-28 14:49:15 -0600

    fix run_scripts directory invoke

--- src/run_scripts/run_all.sh
+++ src/run_scripts/run_all.sh
@@ -23,7 +23,7 @@ mkdir -p ${outdir}almanac
 almanac -v -p 12 --mjd-start $mjd --mjd-end $mjd --${tele} --output $almanac_file
 
 # get the runlist file (julia projects seem to refer to where your cmd prompt is when you call the shell. Here I imaging sitting at ApogeeReduction.jl level)
-julia +1.11.0 --project="./" src/cal_build/make_runlist_all.jl --tele $tele --almanac_file $almanac_file --output $runlist
+julia +1.11.0 --project="./" src/run_scripts/make_runlist_all.jl --tele $tele --almanac_file $almanac_file --output $runlist
 
 # Run the reduction pipeline to 2D/2Dcal and stop
 julia +1.11.0 --project="./" pipeline.jl --tele $tele --runlist $runlist --outdir $doutdir --runname $runname --chips "abc" --caldir_darks "/uufs/chpc.utah.edu/common/home/sdss42/sdsswork/users/u6039752-1/working/2024_09_21/outdir/" --caldir_flats "/uufs/chpc.utah.edu/common/home/sdss42/sdsswork/users/u6039752-1/working/2024_10_03/outdir/" 
