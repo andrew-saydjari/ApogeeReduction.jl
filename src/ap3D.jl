cdae90acbffc37c43aeb9fa1d86a80499aa601c6
Adam Wheeler <awheeler@flatironinstitute.org>
2024-11-04 14:38:39 -0500

    sutr correctly with masked diffs

--- src/ap3D.jl
+++ src/ap3D.jl
@@ -64,176 +64,7 @@ function outlier_mask(dimages; clip_threshold = 20)
         σ = nanzeroiqr(dimages[i, j, :])
         @. mask[i, j, :] &= (dimages[i, j, :] - μ) < (clip_threshold * σ)
     end
-
     mask
-end
-
-"""
-    sutr_tb!(datacube, gainMat, readVarMat; firstind = 1, good_diffs = nothing, n_repeat = 2)
-
-# Arguments
-- `datacube` has shape (npix_x,npix_y,n_reads)
-- `gainMat`: The gain for each pixel (npix_x,npix_y)
-- `read_var_mat`: the read noise (as a variance) for each pixel (npix_x,npix_y)
-
-# Keyword Arguments
-- firstind 
-- good_diffs, if provided, should contain booleans and have shape (npix_x, npix_y, n_reads-1). If
-  not provided, very simple outlier rejection will be done. (See [`outlier_mask`](@ref))
-
-!!! warning
-    Andrew Saydjari reports a bug in this code's error bars and chi2 values 2024_10_17.
-    This mutates datacube. The difference images are written to datacute[:, :, firstindex+1:end]
-
-!!! note
-    This assumes that all images are sequential (ie separated by the same read time).
-
-Created by Kevin McKinnon and sped up by Adam Wheeler. 
-Based on [Tim Brandt's SUTR python code](https://github.com/t-brandt/fitramp).
-"""
-function sutr_tb!(
-        datacube, gainMat, readVarMat; firstind = 1, good_diffs = nothing, n_repeat = 2)
-    # construct the differences images in place, overwriting datacube
-    for i in size(datacube, 3):-1:(firstind + 1)
-        @views datacube[:, :, i] .= gainMat .* (datacube[:, :, i] .- datacube[:, :, i - 1])
-    end
-    # this view is to minimize indexing headaches
-    dimages = view(datacube, :, :, (firstind + 1):size(datacube, 3))
-
-    if isnothing(good_diffs)
-        good_diffs = outlier_mask(dimages)
-    end
-
-    rates = zeros(Float64, size(datacube, 1), size(datacube, 2))
-    final_vars = zeros(Float64, size(datacube, 1), size(datacube, 2))
-    final_chisqs = zeros(Float64, size(datacube, 1), size(datacube, 2))
-
-    npix, ndiffs = size(dimages)[[1, 3]]
-
-    diffs = zeros(Float64, npix, ndiffs)
-    read_var = zeros(Float64, npix)
-    diffs2use = zeros(Bool, npix, ndiffs)
-    d = zeros(Float64, npix, ndiffs)
-
-    theta = ones(Float64, npix, ndiffs + 1)
-    phi = ones(Float64, npix, ndiffs + 1)
-    Phi = zeros(Float64, npix, ndiffs)
-    #PhiD = zeros(Float64, npix, ndiffs)
-    Theta = zeros(Float64, npix, ndiffs)
-    ThetaD = zeros(Float64, npix, ndiffs + 1)
-    scale = Vector{Float64}(undef, npix)
-    beta = Matrix{Float64}(undef, npix, ndiffs - 1)
-    beta_extended = ones(npix, ndiffs)
-    dC = zeros(Float64, npix, ndiffs)
-    A = Vector{Float64}(undef, npix)
-    B = Vector{Float64}(undef, npix)
-    C = Vector{Float64}(undef, npix)
-
-    # define this one here
-    sgn = ones(Float64, npix, ndiffs)
-    sgn[:, 1:2:end] .= -1
-
-    #slice the datacube to analyze each row sequentially to keep runtime down
-    # THIS LOOP IS ENTIRELY NONALLOCATING SO EDIT WITH CARE
-    for c_ind in axes(dimages, 2)
-        # these involve more copying than is really necessary
-        @views diffs .= dimages[:, c_ind, :]
-        @views read_var .= readVarMat[:, c_ind]
-        @views diffs2use .= good_diffs[:, c_ind, :] # shape = (ndiffs, npix)
-        d .= (diffs .* diffs2use)
-
-        # this is done in a loop to be nonallocating
-        for i in axes(rates, 1)
-            @views rates[i, c_ind] = sum(d[i, :]) / sum(diffs2use[i, :])
-        end
-
-        for _ in 1:n_repeat
-            # scale is the same at alpha in the paper, but we divide it out each Elements
-            # in the covariance matrix for stability
-            # The uncertainty and chi squared value will need to be scaled back later.
-            @views scale .= (rates[:, c_ind] .* (rates[:, c_ind] .> 0)) .+ 2 .* read_var
-            @views beta .= -1 .* read_var # the explicit -1 makes it fully broadcast
-            beta ./= scale
-
-            # Mask resultant differences that should be ignored.  This is half
-            # of what we need to do to mask these resultant differences; the
-            # rest comes later.
-            @views beta .*= (diffs2use[:, 2:end] .* diffs2use[:, 1:(end - 1)])
-
-            @views begin # recurence relations
-                # All definitions and formulas here are in the paper.
-                theta[:, 1:2] .= 1
-                for i in 3:(ndiffs + 1)
-                    theta[:, i] .= theta[:, i - 1] .- beta[:, i - 2] .^ 2 .* theta[:, i - 2]
-                end
-
-                phi[:, ndiffs] .= 1
-                for i in (ndiffs - 1):-1:1
-                    phi[:, i] .= phi[:, i + 1] .- beta[:, i] .^ 2 .* phi[:, i + 2]
-                end
-
-                for i in (ndiffs - 1):-1:1
-                    Phi[:, i] .= Phi[:, i + 1] .* beta[:, i] .+
-                                 sgn[:, i + 1] .* beta[:, i] .* phi[:, i + 2]
-                end
-
-                # This one is defined later in the paper and is used for jump
-                # detection and pedestal fitting.
-                #for i in (ndiffs - 1):-1:1
-                #    PhiD[:, i] .= (PhiD[:, i + 1] .+
-                #                   sgn[:, i + 1] .* d[:, i + 1] .* phi[:, i + 2]) .*
-                #                  beta[:, i]
-                #end
-
-                Theta[:, 1] .= -1 .* theta[:, 1] # the explicit -1 makes it fully broadcast
-                for i in 2:(ndiffs - 1)
-                    Theta[:, i] .= Theta[:, i - 1] .* beta[:, i - 1] .+
-                                   sgn[:, i] .* theta[:, i]
-                end
-
-                # the explicit -1 makes it fully broadcast
-                ThetaD[:, 2] .= -1 .* d[:, 1] .* theta[:, 1]
-                for i in 2:(ndiffs - 1)
-                    ThetaD[:, i + 1] .= beta[:, i - 1] .* ThetaD[:, i] .+
-                                        sgn[:, i] .* d[:, i] .* theta[:, i]
-                end
-            end
-
-            beta_extended[:, 2:end] .= beta
-
-            # C' in the paper
-            @views dC .= sgn ./ theta[:, ndiffs + 1] .*
-                         (phi[:, 2:end] .* Theta .+ theta[:, 1:(end - 1)] .* Phi) .*
-                         diffs2use
-
-            # TODO?
-            #dB = sgn ./ theta_ndiffs .*
-            #     (phi[2:end, :] .* ThetaD[2:end, :] .+ theta[1:(end - 1), :] .* PhiD)
-
-            @views begin # {\cal A}, {\cal B}, {\cal C} in the paper
-                A .= 0
-                B .= 0
-                C .= 0
-                for i in 1:ndiffs
-                    A .+= 2 .*
-                          (d[:, i] .* sgn[:, i] .* beta_extended[:, i] .* phi[:, i + 1] .*
-                           ThetaD[:, i] .+
-                           d[:, i] .^ 2 .* theta[:, i] .* phi[:, i + 1]) ./
-                          theta[:, ndiffs + 1]
-                    B .+= d[:, i] .* dC[:, i]
-                    C .+= dC[:, i]
-                end
-            end
-
-            #use first countrate measurement to improve alpha,beta definitions
-            #and extract better, unbiased countrate
-            rates[:, c_ind] .= B ./ C
-            final_vars[:, c_ind] .= scale ./ C
-            final_chisqs[:, c_ind] .= (A .- B .^ 2 ./ C) ./ scale
-        end
-    end
-
-    return rates, final_vars .^ (-1), final_chisqs
 end
 
 """
@@ -306,36 +137,122 @@ function sutr_wood!(datacube, gainMat, readVarMat; firstind = 1, n_repeat = 2)
     d1s = sum(dimages, dims = 3)
     d2s = sum(abs2, dimages, dims = 3)
 
-    # this loop is non-allocating. Edit with care.
     for pixel_ind in CartesianIndices(view(dimages, :, :, 1))
         good_diffs = all_good_diffs[pixel_ind, :]
+        n_good_diffs = sum(good_diffs)
+        if n_good_diffs == ndiffs
+            read_var = readVarMat[pixel_ind]
+            @views mul!(Qdata, Q, view(dimages, pixel_ind, :))
+            d1 = d1s[pixel_ind, 1]
+            d2 = d2s[pixel_ind, 1]
 
-        read_var = readVarMat[pixel_ind]
-        #@views mul!(Qdata[good_diffs], Q[good_diffs, good_diffs],
-        #    view(dimages, pixel_ind, good_diffs))
-        Qdata[good_diffs] .= Q[good_diffs, good_diffs] * dimages[pixel_ind, good_diffs]
-        d1 = sum(dimages[pixel_ind, good_diffs])
-        d2 = sum(abs2, dimages[pixel_ind, good_diffs])
+            Qones = Q * ones_vec
 
-        Qones = Q[good_diffs, good_diffs] * ones_vec[good_diffs]
+            for _ in 1:n_repeat
+                rate_guess = rates[pixel_ind] > 0 ? rates[pixel_ind] : 0
+                x = (rate_guess + 1.5read_var)
+                y = 2 * x / read_var
 
-        for _ in 1:n_repeat
-            rate_guess = rates[pixel_ind] > 0 ? rates[pixel_ind] : 0
-            x = (rate_guess + 1.5read_var)
-            y = 2 * x / read_var
+                Kinv .= D ./ (D .+ y)
+                KinvQones .= Kinv .* Qones
+                KinvQdata .= Kinv .* Qdata
 
-            # TODO don't bother to mask bad pixels here
-            Kinv .= D ./ (D .+ y)
-            KinvQones[good_diffs] .= Kinv[good_diffs] .* Qones
-            KinvQdata[good_diffs] .= Kinv[good_diffs] .* Qdata[good_diffs]
+                ivars[pixel_ind] = (ndiffs - Qones' * KinvQones) / x
+                rates[pixel_ind] = (d1 - Qones' * KinvQdata) / x / ivars[pixel_ind]
+                chi2s[pixel_ind] = (d2 - Qdata' * KinvQdata) / x -
+                                   rates[pixel_ind]^2 * ivars[pixel_ind]
+            end
+        else
+            # the appendix of https://github.com/dfink/gspice/blob/main/paper/gspice.pdf
+            # has a nice derivation of how to invert a matrix with masked elements
+            # this implementation is super naive
 
-            #TODO try sums instead of inner products
-            #TODO Qones is masked already, Qdiffs is not
-            ivars[pixel_ind] = (sum(good_diffs) - Qones' * KinvQones[good_diffs]) / x
-            rates[pixel_ind] = (d1 - Qones' * KinvQdata[good_diffs]) / x / ivars[pixel_ind]
-            chi2s[pixel_ind] = (d2 - Qdata[good_diffs]' * KinvQdata[good_diffs]) / x -
-                               rates[pixel_ind]^2 * ivars[pixel_ind]
+            for _ in 1:n_repeat
+                rate_guess = rates[pixel_ind] > 0 ? rates[pixel_ind] : 0
+                @views C = SymTridiagonal(
+                    (rate_guess + 2readVarMat[pixel_ind]) .* ones_vec, -readVarMat[pixel_ind] .*
+                                                                       ones_vec[1:(end - 1)])[
+                    good_diffs, good_diffs]
+
+                @views ivars[pixel_ind] = ones_vec[1:n_good_diffs]' * (C \
+                                           ones_vec[1:n_good_diffs])
+                @views rates[pixel_ind] = (ones_vec[1:n_good_diffs]' * (C \
+                                            dimages[pixel_ind, good_diffs])) /
+                                          ivars[pixel_ind]
+                @views chi2s[pixel_ind] = (dimages[pixel_ind, good_diffs]' * (C \
+                                            dimages[pixel_ind, good_diffs])) /
+                                          ivars[pixel_ind] -
+                                          rates[pixel_ind]^2 * ivars[pixel_ind]
+            end
         end
     end
     return rates, ivars, chi2s, CRimage
+end
+
+"""
+    submatrix_inv_mult(M, Minv, imask, Y, MinvY; irange=nothing, pad=false, bruteforce=false) -> Ainvy
+
+Given M, Minv, and MinvY, compute inverse of submatrix of M, times Y
+
+# Arguments:
+- `M`:     (Nd,Nd) parent matrix, assumed symmetric and positive semi-definite.
+- `Minv`:  inverse of M, usually calculated with Cholesky.
+- `imask`: mask of rows/columns to use (1=keep, 0=remove). Contains Nk ones and Nr zeros.
+- `Y`:     Matrix (Nspec,Nd) to multiply Ainv by (Nspec can be 1)
+- `MinvY`: Minv times Y
+
+# Output:
+- `Ainvy`: Inverse A (submatrix of M) times Y (Nspec, Nd-Nr). If pad==true, then zero-padded to (Nspec, Nd).
+
+2021-Oct-21 - Originally written by Douglas Finkbeiner, CfA. 
+https://github.com/dfink/gspice/blob/main/julia/gspice.jl
+"""
+function submatrix_inv_mult(M, Minv, imask, Y, MinvY)
+    # COMMENTS:
+    #   Let M by a block matrix
+    #
+    #          | A   B |                      | P   Q |
+    #     M  = |       |      and    M^{-1} = |       |
+    #          | B^T D |                      | Q^T U |
+    #
+    #   Then the inverse of submatrix A is the Schur complement of U,
+    #
+    #     A^{-1} = P - Q U^{-1} Q^T
+    #
+    #   U and M must be invertible and positive semi-definite.
+    #   This function returns
+    #
+    #     A^{-1} Y = P Y - Q U^{-1} Q^T Y
+    #
+
+    @assert size(Y, 1) !== 1     # 'Y must not be a row vector'
+    @assert size(MinvY) == size(Y)
+    @assert length(imask) == size(M, 1)
+
+    # -------- index list to remove
+    r = findall(.!imask)
+    nr = length(r)
+
+    # -------- if there is nothing to do, return early
+    if nr == 0
+        println("imask does not remove any rows/columns...")
+        return MinvY
+    end
+
+    k = findall(imask)
+    Q = Minv[k, r]
+    # -------- Use that Qty = Minv y - U y.
+    U = Symmetric(Minv[r, r])
+    Yr = Y[r, :]
+    Qty = MinvY[r, :] - U * Yr
+
+    # -------- evaluate  A^{-1} Y = P Y - Q U^{-1} Q' Y
+    if length(U) == 1
+        UinvQtY = Qty / U[1]
+    else
+        UinvQtY = cholesky(U) \ Qty
+    end
+
+    AinvY = MinvY[k, :] - (Q * (UinvQtY + Yr))
+    return AinvY
 end
