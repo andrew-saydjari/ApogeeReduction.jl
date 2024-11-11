ff0e3ef9e0fc9e531963f88c96ae31132787564b
Adam Wheeler <wheeler.883@osu.edu>
2024-11-11 13:58:56 -0700

    remove comment

--- pipeline_2d_1d.jl
+++ pipeline_2d_1d.jl
@@ -142,7 +142,7 @@ git_branch, git_commit = initalize_git(src_dir);
         # we probably want to append info from the fiber dictionary from alamanac into the file name
         # outfname = replace(fname, "ap2Dcal" => "ap1D")
         outfname = replace(fname, "ap2D" => "ap1D")
-        jldsave(outfname; flux_1d, ivar_1d, mask_1d, git_branch, git_commit) # ivarimage, pix_bitmask come back and put analogies of these in
+        jldsave(outfname; flux_1d, ivar_1d, mask_1d, git_branch, git_commit)
     end
 end
 t_now = now();
