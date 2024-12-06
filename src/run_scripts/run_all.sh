7abba3588f5de9ba0b1c9bce40e40a5a6e0a0878
andrew-saydjari <saydjari@g.harvard.edu>
2024-12-05 23:45:04 -0700

    fix run_all

--- src/run_scripts/run_all.sh
+++ src/run_scripts/run_all.sh
@@ -17,20 +17,20 @@ doutdir=$outdir
 almanac_file=${outdir}almanac/${runname}.h5
 runlist=${outdir}almanac/runlist_${runname}.jld2
 
-# # set up the output directory (if does not exist)
-# mkdir -p ${outdir}almanac
+# set up the output directory (if does not exist)
+mkdir -p ${outdir}almanac
 
-# # get the data summary file for the MJD
-# almanac -v -p 12 --mjd-start $mjd --mjd-end $mjd --${tele} --output $almanac_file --fibers
+# get the data summary file for the MJD
+almanac -v -p 12 --mjd-start $mjd --mjd-end $mjd --${tele} --output $almanac_file --fibers
 
-# # # get the runlist file (julia projects seem to refer to where your cmd prompt is when you call the shell. Here I imaging sitting at ApogeeReduction.jl level)
-# julia +1.11.0 --project="./" src/run_scripts/make_runlist_all.jl --tele $tele --almanac_file $almanac_file --output $runlist
+# # get the runlist file (julia projects seem to refer to where your cmd prompt is when you call the shell. Here I imaging sitting at ApogeeReduction.jl level)
+julia +1.11.0 --project="./" src/run_scripts/make_runlist_all.jl --tele $tele --almanac_file $almanac_file --output $runlist
 
-# # Run the reduction pipeline to 2D/2Dcal and stop
-# julia +1.11.0 --project="./" pipeline.jl --tele $tele --runlist $runlist --outdir $doutdir --runname $runname --chips "abc" --caldir_darks "/uufs/chpc.utah.edu/common/home/sdss42/sdsswork/users/u6039752-1/working/2024_09_21/outdir/" --caldir_flats "/uufs/chpc.utah.edu/common/home/sdss42/sdsswork/users/u6039752-1/working/2024_10_03/outdir/" 
+# Run the reduction pipeline to 2D/2Dcal and stop
+julia +1.11.0 --project="./" pipeline.jl --tele $tele --runlist $runlist --outdir $doutdir --runname $runname --chips "abc" --caldir_darks "/uufs/chpc.utah.edu/common/home/sdss42/sdsswork/users/u6039752-1/working/2024_09_21/outdir/" --caldir_flats "/uufs/chpc.utah.edu/common/home/sdss42/sdsswork/users/u6039752-1/working/2024_10_03/outdir/" 
 
-# # Extract traces from dome flats
-# ./src/cal_build/run_trace_cal.sh $tele $mjd $mjd
+# Extract traces from dome flats
+./src/cal_build/run_trace_cal.sh $tele $mjd $mjd
 
 # Run pipeline 1D only
 julia +1.11.0 --project="./" pipeline_2d_1d.jl --tele $tele --runlist $runlist --outdir $doutdir --runname $runname
