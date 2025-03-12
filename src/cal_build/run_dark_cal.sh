99c531c96c83bbe7a7586bdf68e18634b0cca500
andrew-saydjari <saydjari@g.harvard.edu>
2025-03-12 15:20:26 -0600

    reduce time request to 8h on the daily scripts, because it should never take even that

--- src/cal_build/run_dark_cal.sh
+++ src/cal_build/run_dark_cal.sh
@@ -6,7 +6,7 @@
 
 #SBATCH --mem=0 #requesting all of the memory on the node
 
-#SBATCH --time=96:00:00
+#SBATCH --time=8:00:00
 #SBATCH --job-name=ApogeeReduction_dark_cal
 #SBATCH --output=slurm_logs/%x_%j.out
 #SBATCH --err=slurm_logs/%x_%j.err
