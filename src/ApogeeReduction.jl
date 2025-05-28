e2ab1079ee8e88f7388322a05b1d9fabd162a963
Kevin McKinnon <kevinmckinnon95@gmail.com>
2025-05-28 16:05:57 -0400

    Update ApogeeReduction.jl

--- src/ApogeeReduction.jl
+++ src/ApogeeReduction.jl
@@ -4,6 +4,8 @@ export N_FIBERS, N_XPIX, N_CHIPS
 const N_FIBERS = 300
 const N_XPIX = 2048
 const N_CHIPS = 3
+const CHIP_LIST = ["a","b","c"]
+const FIRST_CHIP = "a"
 
 include("utils.jl")
 include("ar3D.jl")
