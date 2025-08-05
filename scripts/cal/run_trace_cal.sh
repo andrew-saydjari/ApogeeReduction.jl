5cd91d5367773492328f3e92e8853961adcfb831
Adam Wheeler <wheeler.883@osu.edu>
2025-08-05 09:16:20 -0600

    silence safe_jldsave warning in make_traces+{quartz,dome}flats.jl

--- scripts/cal/run_trace_cal.sh
+++ scripts/cal/run_trace_cal.sh
@@ -55,13 +55,13 @@ timed_exec "Starting almanac data collection" almanac -v -p 12 --mjd-start $mjd_
 timed_exec "Getting domeflat runlist (make_runlist_dome_flats.jl)" julia +1.11.0 --project="./" scripts/cal/make_runlist_dome_flats.jl --tele $tele --almanac_file $almanac_file --output $domerunlist
 timed_exec "Running 3D->2D for domeflats" julia +1.11.0 --project="./" pipeline.jl --tele $tele --runlist $domerunlist --outdir $outdir --runname $runname --chips "RGB" --caldir_darks $caldir_darks --caldir_flats $caldir_flats
 mkdir -p ${outdir}dome_flats
-timed_exec "Domeflat trace generation" julia +1.11.0 --project="./" scripts/cal/make_traces_domeflats.jl --mjd-start $mjd_start --mjd-end $mjd_end --tele $tele --trace_dir ${doutdir} --runlist $domerunlist
+timed_exec "Domeflat trace generation (make_traces_domeflats.jl)" julia +1.11.0 --project="./" scripts/cal/make_traces_domeflats.jl --mjd-start $mjd_start --mjd-end $mjd_end --tele $tele --trace_dir ${doutdir} --runlist $domerunlist
 
 # Parallel reduction for quartz flats
 timed_exec "Getting quartzflat runlist" julia +1.11.0 --project="./" scripts/cal/make_runlist_quartz_flats.jl --tele $tele --almanac_file $almanac_file --output $quartzrunlist
 timed_exec "Running 3D->2D for quartzflats" julia +1.11.0 --project="./" pipeline.jl --tele $tele --runlist $quartzrunlist --outdir $outdir --runname $runname --chips "RGB" --caldir_darks $caldir_darks --caldir_flats $caldir_flats
 mkdir -p ${outdir}quartz_flats
-timed_exec "Quartzflat trace generation" julia +1.11.0 --project="./" scripts/cal/make_traces_quartzflats.jl --mjd-start $mjd_start --mjd-end $mjd_end --tele $tele --trace_dir ${doutdir} --runlist $quartzrunlist
+timed_exec "Quartzflat trace generation (make_traces_quartzflats.jl)" julia +1.11.0 --project="./" scripts/cal/make_traces_quartzflats.jl --mjd-start $mjd_start --mjd-end $mjd_end --tele $tele --trace_dir ${doutdir} --runlist $quartzrunlist
 
 # 2D->1D and relFlux
 timed_exec "Running 2D->1D Pipeline for DomeFlats" julia +1.11.0 --project="./" pipeline_2d_1d.jl --tele $tele --runlist $domerunlist --outdir $outdir --runname $runname --relFlux false
