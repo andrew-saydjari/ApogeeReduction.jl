3b687ad5a98bd2481c0134a955a6e21b7245c0e7
Andrew Saydjari <andrew.saydjari@princeton.edu>
2025-09-17 19:11:02 -0400

    two bug fixes

--- scripts/daily/run_all.sh
+++ scripts/daily/run_all.sh
@@ -100,7 +100,7 @@ if [ ! -f "$almanac_file" ] || $almanac_clobber_mode; then
     #  need to have .ssh/config setup for mwm and a pass_file that is chmod 400
     sshpass -f ~/pass_file ssh -f -N -L 63333:operations.sdss.org:5432 mwm
 
-    almanac -p 12 -v --mjd-start $mjd --mjd-end $mjd  --output $almanac_file --fibers
+    almanac -p 12 -v --$tele --mjd-start $mjd --mjd-end $mjd  --output $almanac_file --fibers
 fi
 
 print_elapsed_time "Building Runlist"
