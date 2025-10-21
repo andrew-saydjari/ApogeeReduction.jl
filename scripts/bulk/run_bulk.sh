ef5964ec6198c1ef3eec83e68daf008f0a319f2f
Andrew Saydjari <andrew.saydjari@princeton.edu>
2025-10-20 20:24:36 -0400

    16 node upgrade

--- scripts/bulk/run_bulk.sh
+++ scripts/bulk/run_bulk.sh
@@ -10,7 +10,7 @@
 # ------------------------------------------------------------------------------
 #SBATCH --partition=cca
 #SBATCH --constraint="[genoa|icelake|rome]"
-#SBATCH --nodes=8
+#SBATCH --nodes=16
 
 #SBATCH --time=2-00:00
 #SBATCH --job-name=ar_bulk
