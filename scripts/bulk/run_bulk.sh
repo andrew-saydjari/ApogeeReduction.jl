02f64a7f4a02e6cc91a6267aa37c62874c9b5f9c
Andrew Saydjari <andrew.saydjari@princeton.edu>
2025-10-26 15:41:45 -0400

    back to 4 nodes for final pass to fix 10 files

--- scripts/bulk/run_bulk.sh
+++ scripts/bulk/run_bulk.sh
@@ -10,7 +10,7 @@
 # ------------------------------------------------------------------------------
 #SBATCH --partition=cca
 #SBATCH --constraint="[genoa|icelake|rome]"
-#SBATCH --nodes=16
+#SBATCH --nodes=4
 
 #SBATCH --time=2-00:00
 #SBATCH --job-name=ar_bulk
