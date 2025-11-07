afcd4a374a5e6e51900c9d75457c9948db2f1ed6
Kevin McKinnon <kevinmckinnon95@gmail.com>
2025-11-07 18:58:53 -0500

    Fix bad pixel mask variable in make_stack_flats.jl

--- scripts/cal/make_stack_flats.jl
+++ scripts/cal/make_stack_flats.jl
@@ -1,6 +1,6 @@
 using JLD2, ProgressMeter, ArgParse, SlackThreads, Glob, StatsBase
 using ApogeeReduction: cal2df, load_gain_maps, get_cal_file, gen_design_mat, grow_msk2d, nanzeromedian, 
-                    nanzeroiqr, safe_jldsave, bad_pix_bits, get_cal_path, dark_pix_bits, bad_flat_pix_bits
+                    nanzeroiqr, safe_jldsave, bad_pix_bits, get_cal_path, bad_dark_pix_bits, bad_flat_pix_bits
 
 include("../../src/makie_plotutils.jl") # TODO: move this to ApogeeReduction.jl
 
@@ -104,7 +104,7 @@ calPath, calFlag = get_cal_path(df_dark, parg["tele"], mjd[1], chip)
 f = jldopen(calPath)
 dark_pix_bitmask = f["dark_pix_bitmask"]
 close(f)
-bad_pix_dark = (dark_pix_bitmask[5:2044, 5:2044] .& dark_pix_bits .!= 0);
+bad_pix_dark = (dark_pix_bitmask[5:2044, 5:2044] .& bad_dark_pix_bits .!= 0);
 
 desc = "Stacking flats for $(parg["tele"]) $(chip) from $(parg["mjd-start"]) to $(parg["mjd-end"])"
 @showprogress desc=desc for (indx, fname) in enumerate(flist)
